dbd62d883bd699e7c10e7a98208f590f9dc67e99
Daniel <dte.biotech46@gmail.com>
2025-06-12 19:01:39 -0400

    added conditions to redirect to signup/login functions based on input

--- menu.cpp
+++ menu.cpp
@@ -13,13 +13,21 @@ void login() {
 int main() {
     int exit = 0;
 
-    while (exit != 0) {
+    while (exit != 1) { // allows for the program to repeat the code if the user wants to continue or if the user makes invalid input
         int answer;
 
-        cout << "Welcome to the Mysterious Website! What do you want to do?\n1. Sign Up\n2. Log In\n";
+        cout << "\nWelcome to the Mysterious Website! What do you want to do (Please input a number)\n1. Sign Up\n2. Log In\n"; // signup/login interface
     
         cin >> answer;
 
-        cout << answer;
+        if (answer == 1) {
+            signup();
+        }
+        else if (answer == 2) {
+            login();
+        }
+        else {
+            cout << "Invalid input, please try again\n\n";
+        }
     }
 }
