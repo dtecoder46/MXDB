ac099900b54e7e42f166d1ed8bd6f82abc54de31
Daniel <dte.biotech46@gmail.com>
2025-06-17 11:16:36 -0400

    stored the username into json

--- signup.cpp
+++ signup.cpp
@@ -7,6 +7,7 @@ int main() {
     string username;
     string password;
     string previous_content;
+    string previous_content2;
 
     cout << "\nEnter your username: ";
     cin >> username;
@@ -17,6 +18,23 @@ int main() {
     ifstream json("usernames.json");
 
     while (getline (json, previous_content)) {
-        cout << previous_content;
+        previous_content2 += previous_content;
     }
+
+    // removes the curly brackets
+
+    previous_content2.replace(0,1,""); 
+    previous_content2.replace(previous_content2.length()-1,1,"");
+
+    char id = previous_content2[5] + 1;
+
+    string new_content = previous_content2 + ",\t\"" + id + "\": \"" + username + "\"";
+
+    string json_str = "{\n" + new_content + "\n}";
+
+    ofstream json2("usernames.json");
+
+    json2 << json_str;
+
+    json2.close();
 }
